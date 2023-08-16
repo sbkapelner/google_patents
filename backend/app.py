3d0d113882b57f9a800cbfabc5de483cc9403a53
sbkapelner <sbkapelner@gmail.com>
2023-08-16 15:15:23 -0400

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,7 @@ def submit():
     }
 
     if err_nodata == "true" or err_datashape == "true":
-        return redirect("http://localhost:3000/")
+        return redirect(f"http://localhost:3000/")
 
     data = request.form["generate"].split("\r\n")
     fname = f"{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.docx"
